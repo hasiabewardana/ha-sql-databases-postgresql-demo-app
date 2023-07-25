c81b6c98805036c2a120e899e409bf088474f99a
hasiabewardana <hasi.prasanga@gmail.com>
2023-07-25 08:05:08 +0530

    Used aggregate functions.

--- ha-database-scripts/SQL_Deep_Dive.sql
+++ ha-database-scripts/SQL_Deep_Dive.sql
@@ -12,10 +12,11 @@ SELECT count(*) - 1
 FROM salaries
 WHERE emp_no = '10001';
 
--- What title does 10006 have
+-- What title does 10006 have?
 SELECT title
 FROM titles
-WHERE emp_no = '10006' AND to_date = '9999-01-01';
+WHERE emp_no = '10006'
+  AND to_date = '9999-01-01';
 
 -- Aliases
 -- Changing column names in a select query
@@ -29,6 +30,28 @@ SELECT emp_no, concat(first_name, ' ', last_name) AS "Full Name"
 FROM employees;
 
 -- Aggregate functions
--- How many people are working in the company
+-- How many people are working in the company?
 SELECT count(emp_no)
 FROM employees;
+
+-- What is the minimum of employee no?
+SELECT min(emp_no)
+FROM employees;
+
+-- What is the maximum of employee no?
+SELECT max(emp_no)
+FROM employees;
+
+-- Get the highest salary available.
+SELECT max(salary)
+FROM salaries;
+
+-- Get the total amount of salaries paid.
+SELECT sum(salary)
+FROM salaries;
+
+-- Exercise: Aggregate Functions
+-- Question 2: What year was the youngest person born in the company?
+-- Table: employees
+SELECT avg(salary)
+FROM salaries;
