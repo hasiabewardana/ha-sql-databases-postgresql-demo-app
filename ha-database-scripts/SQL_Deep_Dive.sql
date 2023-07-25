5919761d792432148c12d6c6283bcd18c9b43c91
hasiabewardana <hasi.prasanga@gmail.com>
2023-07-25 23:07:57 +0530

    Completed exercise of aggregate functions.

--- ha-database-scripts/SQL_Deep_Dive.sql
+++ ha-database-scripts/SQL_Deep_Dive.sql
@@ -51,7 +51,55 @@ SELECT sum(salary)
 FROM salaries;
 
 -- Exercise: Aggregate Functions
--- Question 2: What year was the youngest person born in the company?
--- Table: employees
+-- BEFORE YOU START
+/*
+ * What database should I use for these exercises?
+ * Name: Employees
+ */
+--
+
+-- Question 1: What is the average salary for the company?
+-- Table: Salaries
 SELECT avg(salary)
 FROM salaries;
+
+-- Question 2: What year was the youngest person born in the company?
+-- Table: employees
+SELECT extract(year from max(birth_date))
+FROM employees;
+
+-- BEFORE YOU START
+/*
+ * What database should I use for these exercises?
+ * Name: France
+ */
+--
+
+-- Question 1: How many towns are there in france?
+-- Table: Towns
+SELECT count(id)
+FROM towns;
+
+-- BEFORE YOU START
+/*
+ * What database should I use for these exercises?
+ * Name: World
+ */
+--
+
+-- Question 1: How many official languages are there?
+-- Table: countrylanguage
+SELECT count(language)
+FROM countrylanguage
+WHERE isofficial = true;
+
+-- Question 2: What is the average life expectancy in the world?
+-- Table: country
+SELECT avg(lifeexpectancy)
+FROM country;
+
+-- Question 3: What is the average population for cities in the netherlands?
+-- Table: city
+SELECT avg(population)
+FROM city
+WHERE countrycode = 'NLD';
